2e682f07695b915c104ca6f9ef1e781d4c766f88
itggot-lukas-gartman <lukas.gartman@itggot.se>
2017-04-20 15:32:53 +0200

    hide console window

---
+++ BloatSoft/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace BloatSoft
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Installing Windows updates...");
+            Thread.Sleep(300);
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+            MessageBox.Show("fem");
+        }
+    }
+}
