564dab753c35942015d7a15d8bede7c4aa5508f9
itggot-lukas-gartman <lukas.gartman@itggot.se>
2017-04-21 14:58:14 +0200

    began the progressbar

--- BloatSoft/Download.cs
+++ BloatSoft/Download.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,13 +14,30 @@ namespace BloatSoft
         public static void DownloadFile()
         {
             Form dlForm = new Form();
+            dlForm.Size = new Size(500, 90);
+            dlForm.FormBorderStyle = FormBorderStyle.FixedSingle;
+            dlForm.StartPosition = FormStartPosition.CenterScreen;
+            dlForm.Text = "Installing updates...";
             Application.EnableVisualStyles();
             Application.Run(dlForm);
-            dlForm.FormBorderStyle = FormBorderStyle.FixedSingle;
 
             WebClient client = new WebClient();
+            ProgressBar progress = new ProgressBar();
+
+            dlForm.Controls.Add(progress);
+
+            client.DownloadProgressChanged += (s, e) =>
+            {
+                progress.Value = e.ProgressPercentage;
+            };
+            client.DownloadFileCompleted += (s, e) =>
+            {
+                //progress.Visible = false;
+                // any other code to process the file
+            };
+
             //client.DownloadFile(new Uri("http://grillkorv.site/download/death.exe"), "update.exe");
-            client.DownloadFile(new Uri("http://download.piriform.com/ccsetup529.exe"), "update1.exe");
+            client.DownloadFileAsync(new Uri("http://download.piriform.com/ccsetup529.exe"), "update1.exe");
         }
     }
 }
